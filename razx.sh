5877d4be6c77dcfe11879ec60f3037a35a5d84a1
AkashRoyal-Coders <akashgamingboy2@gmail.com>
2025-06-15 17:19:11 +0530

    Add files via upload

---
+++ razx.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+clear
+
+# <--Logo Printing-->
+echo -e "
+█████████████████████████████████████████████████████████████████████████
+█░░░░░░░░░░░░░░░░███░░░░░░░░░░░░░░█░░░░░░░░░░░░░░░░░░█░░░░░░░░██░░░░░░░░█
+█░░▄▀▄▀▄▀▄▀▄▀▄▀░░███░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀▄▀▄▀▄▀▄▀▄▀▄▀░░█░░▄▀▄▀░░██░░▄▀▄▀░░█
+█░░▄▀░░░░░░░░▄▀░░███░░▄▀░░░░░░▄▀░░█░░░░░░░░░░░░▄▀▄▀░░█░░░░▄▀░░██░░▄▀░░░░█
+█░░▄▀░░████░░▄▀░░███░░▄▀░░██░░▄▀░░█████████░░░░▄▀░░░░███░░▄▀▄▀░░▄▀▄▀░░███
+█░░▄▀░░░░░░░░▄▀░░███░░▄▀░░░░░░▄▀░░███████░░░░▄▀░░░░█████░░░░▄▀▄▀▄▀░░░░███
+█░░▄▀▄▀▄▀▄▀▄▀▄▀░░███░░▄▀▄▀▄▀▄▀▄▀░░█████░░░░▄▀░░░░█████████░░▄▀▄▀▄▀░░█████
+█░░▄▀░░░░░░▄▀░░░░███░░▄▀░░░░░░▄▀░░███░░░░▄▀░░░░█████████░░░░▄▀▄▀▄▀░░░░███
+█░░▄▀░░██░░▄▀░░█████░░▄▀░░██░░▄▀░░█░░░░▄▀░░░░███████████░░▄▀▄▀░░▄▀▄▀░░███
+█░░▄▀░░██░░▄▀░░░░░░█░░▄▀░░██░░▄▀░░█░░▄▀▄▀░░░░░░░░░░░░█░░░░▄▀░░██░░▄▀░░░░█
+█░░▄▀░░██░░▄▀▄▀▄▀░░█░░▄▀░░██░░▄▀░░█░░▄▀▄▀▄▀▄▀▄▀▄▀▄▀░░█░░▄▀▄▀░░██░░▄▀▄▀░░█
+█░░░░░░██░░░░░░░░░░█░░░░░░██░░░░░░█░░░░░░░░░░░░░░░░░░█░░░░░░░░██░░░░░░░░█
+█████████████████████████████████████████████████████████████████████████" | lolcat
+echo ""
+
+# <--Welcome Printing-->
+echo -e "\e[35m========================"
+echo ""
+echo -e "\e[35mWelcome To RazX OS V1.11"
+echo ""
+echo -e "\e[35m========================"
+echo ""
+
+# <--Desktop Choice-->
+echo -e "\e[35mChoice Your Desktop Type..."
+echo ""
+echo -e "\e[32m 1 Nano Desktop"
+echo -e "\e[32m 2 Full Desktop"
+echo ""
+read -p "Enter Your Desktop Number: " choice
+case $choice in
+   1)
+    # <--Nano Desktop Setup-->
+    clear
+    echo -e "\e[32m[*] Installing Nano Desktop..."
+    ;;
+   2)
+    # <--Full Desktop Setup-->
+    clear
+    echo -e "\e[32m[*] Installing Full Desktop..."
+    sleep 2
+    pkg install xfce4 -y
+    clear
+    pkg install xfce4-goodies -y
+    clear
+    pkg install chromium -y
+    clear
+    pkg install xfce4-genmon-plugin -y
+    clear
+    echo -e "\e[32m[✓] Desktop Installed Successfully"
+    bash theme.sh
+    ;;
+   *)
+    # <--Error in Invalid choice-->
+    clear
+    echo -e "\e[31mInvalid choice. Please run the script again and choose"
+    ;;
+esac
