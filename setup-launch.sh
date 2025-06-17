5e05214b5aa5c3d0bff99645f8bd9fc6e5bf42db
AkashRoyal-Coders <akashgamingboy2@gmail.com>
2025-06-17 08:57:57 +0530

    Update setup-launch.sh

--- setup-launch.sh
+++ setup-launch.sh
@@ -2,7 +2,7 @@
 
 termux-x11 :1 &
 export DISPLAY=:1
-xfce4-session &
 am start -n com.termux.x11/.MainActivity &
-python /data/data/com.termux/files/home/RazX/dependencies/system/boot/main.py
-python /data/data/com.termux/files/home/RazX/Installer-resources/setup/main.py
+python /data/data/com.termux/files/home/RazX/dependencies/system/boot/main.py &
+python /data/data/com.termux/files/home/RazX/Installer-resources/setup/main.py &
+xfce4-session
